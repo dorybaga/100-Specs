f21246feba77cbc0f9593365143632fc5cb147b8
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-17 11:09:00 -1000

    DNA arr

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -224,6 +224,8 @@ var linuxFlavors = ["Gentoo", "Fedora", "Debian", "Slackware", "Red Hat", "Biebe
  *
  */
 
+var DNA = ["nucleatides", "guanine", "adenine", "thymine", "cytosine"];
+
 
 /* Step 16
  *
