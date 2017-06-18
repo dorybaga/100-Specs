b5326c7f65136f9c2c2dce1d4a0ab340a5fe6409
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-17 18:34:35 -1000

    made new Doors

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1208,8 +1208,8 @@ var catBox = new Box(cat, true);
 var christmasPresent = new Box("gift", false);
 
 // Create 2 doors
-var automaticDoor;
-var bankVault;
+var automaticDoor = new Door(true);
+var bankVault = new Door(false);
 
 // Create 2 shoes
 var rubySlippers;
