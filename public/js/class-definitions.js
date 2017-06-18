8df52b4efa4927d7199347364df29426f4c50277
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-06-17 19:36:03 -1000

    flip da switch can

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1415,6 +1415,15 @@ House.prototype.isATallStory = function(storiesTooTall){
  *
  */
 
+Lightbulb.prototype.flipSwitch = function(on){
+  if (on === "on"){
+    this.isOn = true;
+    return true;
+  } else {
+    this.isOn = false;
+    return false;
+  }
+};
 
  /* Step 89
  *
